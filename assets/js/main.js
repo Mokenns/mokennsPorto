07a17d886288eab6dd62ecbe26d4c1b6371b5155
Mokens <miquiangelhotmail@gmail.com>
2024-12-12 16:18:36 -0300

    language changer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ import updateCopyRightYear from './helpers/date_updater.js';
 import loader from './helpers/loader.js';
 import resetToHome from './helpers/reload_page.js';
 import sendEmail from './helpers/send_form.js';
+import changeLanguage from './helpers/language.js';
 
 loader();
 resetToHome();
@@ -11,3 +12,4 @@ parallax();
 activeMenu();
 updateCopyRightYear();
 sendEmail();
+changeLanguage();
