dfbb8b69777a114acd1ed11bb893985d28c327ca
Mokens <miquiangelhotmail@gmail.com>
2024-12-12 16:28:49 -0300

    fixed language

--- assets/js/helpers/language.js
+++ assets/js/helpers/language.js
@@ -1,9 +1,17 @@
 function changeLanguage() {
 	const toggleSwitch = document.getElementById('language-toggle');
+
+	const savedLanguage = localStorage.getItem('language');
+	if (savedLanguage === 'en') {
+		toggleSwitch.checked = true;
+	}
+
 	toggleSwitch.addEventListener('change', () => {
 		if (toggleSwitch.checked) {
+			localStorage.setItem('language', 'en');
 			window.location.href = 'index-en.html';
 		} else {
+			localStorage.setItem('language', 'default');
 			window.location.href = 'index.html';
 		}
 	});
